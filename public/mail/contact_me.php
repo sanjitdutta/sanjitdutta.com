e91b0cde8555cee4880c8f1bfaa276d945b9b8f3
Sanjit Dutta <sanjit.dutta@pwc.com>
2020-01-28 00:26:20 -0500

    add timeline and contact

---
+++ public/mail/contact_me.php
@@ -0,0 +1,46 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	die("false");
+   }
+
+// recaptcha stuff
+   
+if(empty($_POST['g-recaptcha-response'])) die("false");
+
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$data = array('secret' => '6LfaBgsTAAAAACG6XGoQ5Hfw6OGcM2XlpfZPsfuq', 'response' => $_POST['g-recaptcha-response']);
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ),
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+$json = json_decode($result);
+if(!$json->success) die("false");;
+   
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'sanjit@sanjitdutta.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@sanjitdutta.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+echo "true";
+?>
