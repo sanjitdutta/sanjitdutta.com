ab4306f75b3b7d5eab10a7da30a48adcd6045948
Sanjit Dutta <sdlynx@gmail.com>
2020-07-03 18:02:11 -0400

    replace to use local Feedr

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sanjitdutta.com
+
+**Note:** this website _unfortunately_ relies on the [Feedr](https://github.com/sdepold/feedrapp) app. You'll need to clone the app, install it, and set it up to run on sanjitdutta.com/feedr/. I've done this/am planning to do this using an Apache reverse proxy.
--- public/js/index.js
+++ public/js/index.js
@@ -4,31 +4,32 @@ $(function() {
    * Contact form animations
    */
 
-  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-  }).on("focus", ".floating-label-form-group", function() {
-    $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function() {
-    $(this).removeClass("floating-label-form-group-with-focus");
+  $('body').on('input propertychange', '.floating-label-form-group', function(e) {
+    $(this).toggleClass('floating-label-form-group-with-value', !!$(e.target).val());
+  }).on('focus', '.floating-label-form-group', function() {
+    $(this).addClass('floating-label-form-group-with-focus');
+  }).on('blur', '.floating-label-form-group', function() {
+    $(this).removeClass('floating-label-form-group-with-focus');
   });
 
   /**
    * Wordpress RSS Feed Loader
    */
-  $("#rss-feeds").rss("https://www.sanjitdutta.com/blog/feed/", {
+  $('#rss-feeds').rss('https://www.sanjitdutta.com/blog/feed/', {
     limit: 2,
+    host: 'www.sanjitdutta.com/feedr/',
     ssl: true,
     support: false,
-    layoutTemplate: "<div class='feed-container'>{entries}</div>",
-    entryTemplate: "<div class='feed-post'> \
-      <h3><a href='{url}' target='_blank'>{title}</a></h3> \
-      <h3 class='date'>{date}</h3> \
+    layoutTemplate: '<div class=\'feed-container\'>{entries}</div>',
+    entryTemplate: '<div class=\'feed-post\'> \
+      <h3><a href=\'{url}\' target=\'_blank\'>{title}</a></h3> \
+      <h3 class=\'date\'>{date}</h3> \
       {teaserImage} \
-      <p>{mediumBodyPlain} <a href='{url}' target='_blank'>more</a></p> \
-    </div>",
-    dateFormat: "MMMM Do, YYYY",
+      <p>{mediumBodyPlain} <a href=\'{url}\' target=\'_blank\'>more</a></p> \
+    </div>',
+    dateFormat: 'MMMM Do, YYYY',
     tokens: {
-      mediumBodyPlain: function(entry, tokens) {
+      mediumBodyPlain: function(entry) {
         var plainText = $(entry.content).text();
         return plainText.substring(0, 300) + '...';
       },
